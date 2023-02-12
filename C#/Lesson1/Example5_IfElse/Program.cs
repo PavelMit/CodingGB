eddd8da2d24715131996155476136b803223ea66
Pavel Mitrofanov <mitrofanovpavel96@gmail.com>
2023-02-12 19:35:30 +0500

    Example5 added

---
+++ C#/Lesson1/Example5_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}

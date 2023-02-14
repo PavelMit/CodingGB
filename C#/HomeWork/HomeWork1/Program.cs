5ce90857484b990571d797d76dcc946a434a030c
Pavel Mitrofanov <mitrofanovpavel96@gmail.com>
2023-02-14 13:45:46 +0500

    Seminar 2 adn HomeWork1 added

---
+++ C#/HomeWork/HomeWork1/Program.cs
@@ -0,0 +1,93 @@
+﻿/* Task 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+/*
+Console.Write("Input an integer number 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input an integer number 1: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int bigNum = num2;
+if (num1 > num2)
+{
+    bigNum = num1;
+}
+Console.WriteLine("The max number is " + bigNum);
+*/
+
+/*Task 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+/*
+Console.Write("Input an integer number 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input an integer number 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input an integer number 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int bigNum = num1;
+
+if (num2 > num1)
+{
+    bigNum = num2;
+}
+if (num3 > bigNum)
+{
+    bigNum = num3;
+}
+
+Console.WriteLine("The max number is " + bigNum);
+*/
+
+/* Task 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+/*
+Console.Write("Input an integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
+*/
+
+/* Task 4.  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+/*
+Console.Write("Input an integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while (current <= num)
+{
+    Console.Write(current);
+    if (current < num)
+    {
+        Console.Write(", ");
+    }
+    current++;
+}
+*/
+

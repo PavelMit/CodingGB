863bdcb2f7efd1a678f5d1e80ce2df98045ef363
Pavel Mitrofanov <mitrofanovpavel96@gmail.com>
2023-02-24 13:40:57 +0500

    Seminar5, HomeWork 5 and Knight of rose petals added

---
+++ Just For Fun/Knight of rose petals/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+// создание кубика
+void EmptyDice(int x, int y)
+{
+    for (int yCurrent = 0; yCurrent <= 10; yCurrent += 2)
+    {
+        if (yCurrent == 0 || yCurrent == 10)
+        {
+            for (int xCurrent = 0; xCurrent <= 20; xCurrent += 4)
+            {
+                Console.SetCursorPosition(x + xCurrent, y + yCurrent);
+                Console.Write("*");
+            }
+        }
+        else
+        {
+            Console.SetCursorPosition(x, y + yCurrent);
+            Console.Write("*");
+            Console.SetCursorPosition(x + 20, y + yCurrent);
+            Console.Write("*");
+        }
+    }
+}
+void Dice(int number, int x, int y)
+{
+    EmptyDice(x, y);
+    if (number == 1)
+    {
+        Console.SetCursorPosition(x + 10, y + 5);
+        Console.Write("X");
+    }
+}
+
+
+Dice(1, 1, 1);
+
+
+// создание кубика c определенным количество точек
